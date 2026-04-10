68134064c9d929a65505766a2fbff3ea7de4f2cf
Tomas Jefanovas <tomas.jefanovas@gmail.com>
2026-04-10 15:39:55 +0100

    bookmarks added

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,6 +1,7 @@
 class List < ApplicationRecord
   validates :name, presence: true, uniqueness: true
   has_one_attached :photo
+
   has_many :bookmarks, dependent: :destroy
   has_many :movies, through: :bookmarks
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  # get "bookmarks/new"
   root "lists#index"
-  resources :lists, only: [ :index, :show, :new, :create ]
+  resources :lists, only: [ :index, :show, :new, :create ] do
+    resources :bookmarks, only: [ :new, :create ]
+  end
+  resources :movies
 end
