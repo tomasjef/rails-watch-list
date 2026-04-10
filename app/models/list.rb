93b42c4ee6c41936b3e0f5d4f3e2b349556b4d09
Tomas Jefanovas <tomas.jefanovas@gmail.com>
2026-04-10 12:09:01 +0100

    setup

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,6 +1,6 @@
 class List < ApplicationRecord
   validates :name, presence: true, uniqueness: true
-
+  has_one_attached :photo
   has_many :bookmarks, dependent: :destroy
   has_many :movies, through: :bookmarks
 end
